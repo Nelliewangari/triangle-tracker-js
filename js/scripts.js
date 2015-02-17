74f59e7834ba9c0c03411ad22b95ae06d16b37ae
Debbie Cravey and Karvari Ellingson <student@epicodus.com>
2015-02-17 12:32:09 -0800

    Add functionality for entering several triangle sides and listing results

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ var triangle = function(sideOne, sideTwo, sideThree){
   var sides = [sideOne, sideTwo, sideThree]
 
   if(sideOne === '' || sideTwo === '' || sideThree === '') {
-    return "Please enter three side lengths";
+    alert("Please enter three side lengths");
   } else if (sides[0] >= (sides[1] + sides[2]) || sides[1] >= (sides[2] + sides[0]) || sides[2] >= (sides[0] + sides[1])) {
-    return "This is not a valid triangle";
+    alert("This is not a valid triangle");
   } else if (sides[0]===sides[1] && sides[1]===sides[2]) {
-    return "Equilateral";
+    return "equilateral";
   } else if (sides[0]===sides[1] || sides[0]===sides[2] || sides[2]===sides[1]) {
-    return "Isosceles";
+    return "isosceles";
   } else if (sides[0]!=sides[1] && sides[0]!=sides[2]) {
-    return "Scalene";
+    return "scalene";
   } else return sides; {;
   }
 };
@@ -23,10 +23,21 @@ $(document).ready(function() {
     var sideOne = parseInt($("input#sideOne").val());
     var sideTwo = parseInt($("input#sideTwo").val());
     var sideThree = parseInt($("input#sideThree").val());
-
     var result = triangle(sideOne, sideTwo, sideThree);
+    var sides = [sideOne, sideTwo, sideThree];
 
-    $(".type-of-triangle").text(result);
+
+    if (result === "equilateral") {
+
+    $("ul#tri-results-equilateral").append("<li>" + sides + "</li>");
+    } else if (result === "isosceles") {
+    $("ul#tri-results-isosceles").append("<li>"+ sides + "</li>");
+    } else if (result === "scalene") {
+    $("ul#tri-results-scalene").append("<li>" + sides + "</li>");
+    } else {
+      alert("Don't be a square, Give me a triangle!")
+    }
+
 
     $("#result").show();
     event.preventDefault();
